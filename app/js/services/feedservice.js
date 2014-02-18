5a265132f744902a58c291d5ea23b7ba2f278485
Piyush Poddar <ppod1991@gmail.com>
2014-02-17 19:11:02 -0500

    Fixed menu toggle

--- app/js/services/feedservice.js
+++ app/js/services/feedservice.js
@@ -4,10 +4,10 @@ angular.module('fantasyApp.services.feed', ['fantasyApp.services.firebaseRefs'])
     .factory('Feed', ['angularFireCollection', 'FireRef', 
         function(angularFireCollection, FireRef) {
             return {
-                collection: function(user, cb) {
-                    //console.log(user);
+                collection: function(auth, cb) {
+                    //console.log(auth);
                     //console.log(FireRef);
-                    return angularFireCollection(FireRef.friendRequests(user.id),cb);
+                    return angularFireCollection(FireRef.friendRequests(auth.id),cb);
 
                     // return angularFireCollection(FireRef.feed(),cb);
                 }
