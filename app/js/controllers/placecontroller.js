8c4e571a44b8f91bb006fb945e3f4de1ad12a05b
Piyush Poddar <ppod1991@gmail.com>
2014-07-03 12:56:23 -0400

    Fixes

--- app/js/controllers/placecontroller.js
+++ app/js/controllers/placecontroller.js
@@ -3,12 +3,15 @@
 angular.module('fantasyApp.controllers.place', ['fantasyApp.services.place'])
   .controller('PlaceCtrl', ['$scope', '$routeParams', 'placeService',
     function($scope, $routeParams, placeService) {
-    	$scope.place_id = $routeParams.store_name;
+    	//$scope.place_id = $routeParams.store_name;
     	//var storeParams = placeService.getStoreParameters($scope.place_id);
     	// $scope.title = storeParams.store_name;
     	// $scope.hashtag_text = storeParams.hashtag_text;
     	// $scope.picture_URL = storeParams.store_picture_URL;
-    	$scope.title = 'Woo title';
-    	$scope.hashtag_text = 'Woo HashtagText';
-    	$scope.picture_URL = 'www.wooURL.com';
-    }])
+    	$scope.meta = {
+    		title: "Woo Title",
+    		hashtag_text: "Woo hashtag text",
+    		picture_URL: "www.wooURL.com"
+    	};
+    }]);
+
--- web-server.js
+++ web-server.js
@@ -60,7 +60,7 @@ passport.use(new FacebookStrategy({
 var app = express();
 
 
-
+app.use(require('prerender-node').set('prerenderToken', 'Diu6USu9BaTRhnmuaDYJ'));
 app.use(logfmt.requestLogger());
 app.use(cookieParser());
 app.use(bodyParser());
@@ -69,7 +69,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/app'));
 app.use(cors());
-app.use(require('prerender-node').set('prerenderToken', 'Diu6USu9BaTRhnmuaDYJ'));
+
 
 
 // app.all('*', function(req, res, next) {
