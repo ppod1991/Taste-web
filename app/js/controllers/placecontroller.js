94afb69570c6b7c6c38bc251d13a9d41b246ab13
Piyush Poddar <ppod1991@gmail.com>
2014-07-03 15:52:17 -0400

    Changes

--- app/js/controllers/placecontroller.js
+++ app/js/controllers/placecontroller.js
@@ -8,7 +8,7 @@ angular.module('fantasyApp.controllers.place', ['fantasyApp.services.place'])
     	console.log(params.store_id);
     	$scope.place_id = params.store_id;
     	console.log("Place ID: " + $scope.place_id);
-    	placeService.setNewStoreParameters($scope.place_id);
+    	$scope.childMeta = placeService.setNewStoreParameters($scope.place_id);
     	// $scope.title = storeParams.store_name;
     	// $scope.hashtag_text = storeParams.hashtag_text;
     	// $scope.picture_URL = storeParams.store_picture_URL;
--- app/js/services/placeservice.js
+++ app/js/services/placeservice.js
@@ -9,6 +9,8 @@ angular.module('fantasyApp.services.place', ['ngResource'])
         		setNewStoreParameters: function(place_id) {
                     var storeParams= $resource("http://desolate-plateau-4658.herokuapp.com/stores/" + place_id).get(function () {
                         console.log("Set New Store Parameters called with place_id: " + place_id);
+                        console.log("Store Params:");
+                        console.log(storeParams);
                         meta = {
                             title: storeParams.store_name,
                             hashtag_text: storeParams.hashtag_text,
