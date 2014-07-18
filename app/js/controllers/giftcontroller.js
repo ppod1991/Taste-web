bb390c9080b333610a6def5d8be0e9da53488974
Piyush Poddar <ppod1991@gmail.com>
2014-07-18 11:59:12 -0400

    Redeem promotions working

--- app/js/controllers/giftcontroller.js
+++ app/js/controllers/giftcontroller.js
@@ -5,18 +5,15 @@ angular.module('fantasyApp.controllers.gift', ['fantasyApp.services.gift','fanta
         function ($scope, giftService, loginService) {
 
             $scope.gifts = {};
-
+            var user_id;
             var promise = loginService.isLoggedIn();
             promise.then(function(value) {
-                var user_id = value;
+                user_id = value;
                 console.log("Gift Controller User Id: " + user_id);
                 if (!(user_id === 0)) {
                     console.log("Querying Gifts with user_id: " + user_id);
-                    giftService.get({use_status: "not used", user_id: user_id},function(response) {
-                        console.log("Gift Controller--returned promotions");
-                        console.log(response);
-                        $scope.gifts = response.Promotions;
-                    });
+                    refreshGifts(user_id);
+
                 }
                 else {
                     console.log("Cannot retrieve gifts as user not logged in");
@@ -25,10 +22,28 @@ angular.module('fantasyApp.controllers.gift', ['fantasyApp.services.gift','fanta
             },null,null);
 
 
+            var refreshGifts = function (id) {
+                giftService.resource.get({use_status: "not used", user_id: id},function(response) {
+                    console.log("Gift Controller--returned promotions");
+                    console.log(response);
+                    $scope.gifts = response.Promotions;
+                });
+            };
+
+            $scope.convertDate = function(dbDate) {
+                var date = new Date(dbDate);
+                return (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear();
+            };
 
 
-            $scope.redeemPromotion = function(user) {
+            $scope.redeemPromotion = function(giftIndex) {
                 console.log("Promoton being redeemed!");
+                var toRedeem = $scope.gifts[giftIndex];
+                giftService.redeem.save(toRedeem, function(value, responseHeader) {
+                    console.log("Response from trying to redeem promotion");
+                    console.log(value);
+                    refreshGifts(user_id);
+                })
                 // var newUser = new userTest();
                 // newUser.first_name = user.first_name;
                 // newUser.last_name = user.last_name;
--- app/js/services/giftservice.js
+++ app/js/services/giftservice.js
@@ -4,6 +4,9 @@ angular.module('fantasyApp.services.gift', ['ngResource'])
     .factory('giftService', ['$resource',
         function($resource) {
             //return $resource("http://desolate-plateau-4658.herokuapp.com/users/:user_id",{user_id:'@user_id'});            
-            console.log("Returning gift resource");
-            return $resource("/promotions");                    
+            //console.log("Returning gift resource");
+            return {
+                resource: $resource("/promotions"),
+                redeem: $resource("/promotions/redeem")
+                };                 
         }])
